dd64b606e6cb1acdfaf86e25fec17bb49dd2d29e
niaDev <estefania.atencioramirez@gmail.com>
2024-07-01 23:31:56 -0300

    Corrigiendo comentarios

--- js/main.js
+++ js/main.js
@@ -1,80 +1,3 @@
-//Datos cliente
-//Constantes
-//const clientes = []
-
-//Variables declaradas
-let firstName, lastname, age, dni, email, amount, installments, cliente
-let continuar = true
-
-// function main() {
-
-//     while (continuar){
-        
-//         dataCollecting()
-//         console.table(cliente)
-//         continuar = confirm("¿Quieres realizar otra consulta?")
-//         cliente = ""
-//     }
-
-//     alert("Muchas gracias por tu consulta")
-// }
-
-// function dataCollecting() {
-// firstName = prompt("Ingrese su nombre")
-// lastname = prompt("Ingrese su apellido")
-// age = prompt("Ingrese su edad") //si no es mayor no continua el proceso
-
-
-//     if (age < 18) {
-//        console.log("Necesitas ser mayor de edad para continuar")
-//        alert("No puedes continuar el proceso")
-//     } else {
-//         dni = prompt("Ingrese su DNI")
-//         email = prompt("Ingrese su correo eléctronico")
-//         let paymentInstallments = división()
-//         console.log("El monto total a pagar por cuotas es "+paymentInstallments)  
-//         alert("El monto total a pagar por cuotas es "+paymentInstallments)
-//         addElement()
-//     }
-// }
-
-// //función para saber el monto total a pagar de las cuotas
-//  function división() {
-//      amount = parseInt(prompt("Ingrese el monto a prestar"))
-//      installments = parseInt(prompt("¿En cuántas cuotas quieres pagar?"))
-//      let totalInstallments = amount / installments
-
-//     return totalInstallments
-//  }
- 
-// //función para añadir otro cliente/consulta
-
-// const addElement = () => {
-    
-//     cliente = {
-//         firstName: firstName,
-//         lastname: lastname,
-//         age: age,
-//         dni: dni,
-//         email: email,
-//         amount: amount,
-//         installments: installments
-//     }
-//     clientes.push(cliente)
-// }
-
-// main()
-
-//Validaciones
-
-// if (document.getElementById('rad1').checked) {
-// } else {
-//     alert("Necesitas ser mayor de edad para continuar")
-//     return false 
-// }
-
-
-
 //clase reportes, objetos
 let clientes = []
 
@@ -131,8 +54,6 @@ let miReporte = document.getElementById("miReporte")
 miReporte.addEventListener("submit", ingresarDatos)
 
 document.getElementById("reporteCompleto").style.display = "none"
-
-
 
 
 
